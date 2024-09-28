f23911d20dc004814efc813c9d63f2254fe2f11f
dingrui <Bannirui@outlook.com>
2024-09-28 16:47:06 +0800

    linux编译

--- deps/jemalloc/build_jemalloc.sh
+++ deps/jemalloc/build_jemalloc.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-./configure --with-version=5.1.0-0-g0 --with-lg-quantum=3 --with-jemalloc-prefix=je_ C
+./configure \
+  --with-version=5.1.0-0-g0 \
+  --with-lg-quantum=3 \
+  --with-jemalloc-prefix=je_
 make lib/libjemalloc.a
